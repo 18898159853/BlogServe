adce929e6d28d137566f455957b53dbabe101221
‘jiaoyou’ <18898159853@163.com>
2024-01-06 16:37:14 +0800

    分享

---
+++ src/components/codes/index.js
@@ -0,0 +1,136 @@
+// 前端
+export const frontEnd = [{
+  name: 'Vue2.js',
+  logo: 'vue.svg',
+  url: 'https://v2.cn.vuejs.org/',
+  text: '渐进式JavaScript 框架。'
+},
+{
+  name: 'Vue3.js',
+  logo: 'vue.svg',
+  url: 'https://cn.vuejs.org/',
+  text: '易学易用，性能出色，适用场景丰富的 Web 前端框架。'
+},
+{
+  name: 'React.js',
+  logo: 'react.svg',
+  url: 'https://reactjs.org/',
+  text: 'React 是一个用于构建用户界面的 JavaScript 库。它由 Facebook 开发并开源，用于构建用户界面。React 可以使你构建大型、复杂的单页应用，而且只使用了 JavaScript 作为编程语言。'
+},
+
+{
+  name: 'Angular.js',
+  logo: 'angular.svg',
+  url: 'https://angular.io/',
+  text: 'Angular 是一个开源的 JavaScript 客户端应用框架，使用 TypeScript 语言进行开发。Angular 的目标是解决前端开发者在日常工作中遇到的问题，如模板的使用、脏检查和路由等。它使用了一个组件化的方法来构建单页应用，这使得开发、测试和重构变得更加容易。'
+},
+{
+  name: 'Webpack',
+  logo: 'webpack.png',
+  url: 'https://webpack.dev/',
+  text: 'Webpack 是一个静态模块打包器，主要用于将模块化的代码打包成浏览器可以执行的 JavaScript 文件。它能够处理图片、CSS、字体等资源文件，并支持热更新和代码分割等功能。Webpack 的配置灵活，可以满足各种复杂的需求。'
+},
+{
+  name: 'Vite',
+  logo: 'vite.svg',
+  url: 'https://vitejs.cn/',
+  text: '下一代前端开发与构建工具'
+},
+{
+  name: 'Axios',
+  logo: 'axios.svg',
+  url: 'https://axios-http.com/',
+  text: 'Axios 是一个基于 Promise 的 HTTP 客户端，可以在浏览器和 Node.js 中使用。'
+},
+{
+  name: 'Element-ui',
+  logo: 'element.svg',
+  url: 'https://element.eleme.cn/#/zh-CN',
+  text: 'Element，一套为开发者、设计师和产品经理准备的基于 Vue 2.0 的桌面端组件库'
+},
+{
+  name: 'Element-Plues',
+  logo: 'element.svg',
+  url: 'https://element-plus.org/zh-CN/#/zh-CN',
+  text: '基于 Vue 3，面向设计师和开发者的组件库'
+},
+{
+  name: 'Ant Design',
+  logo: 'ant.svg',
+  url: 'https://ant.design/docs/react/introduce-cn',
+  text: '助力设计开发者「更灵活」地搭建出「更美」的产品，让用户「快乐工作」～'
+},
+{
+  name: 'Three.js',
+  logo: 'three.png',
+  url: 'http://www.webgl3d.cn/',
+  text: '是一个基于 JavaScript 的开源 3D 图形库'
+},
+{
+  name: 'Vant4 Ul',
+  logo: 'vant.png',
+  url: 'http://mui.ucmed.cn/#/zh-CN/intro',
+  text: '轻量、可定制的移动端 Vue 组件库'
+},
+{
+  name: 'Naive Ul',
+  logo: 'naive.svg',
+  url: 'https://www.naiveui.com/zh-CN/light',
+  text: '一个 Vue 3 组件库'
+},
+{
+  name: 'Iconfont',
+  logo: 'iconfont.png',
+  url: 'https://www.iconfont.cn/',
+  text: '原创设计 海量素材 专业原创团队 千万级图标库'
+},
+{
+  name: '前端资料',
+  logo: 'logo.png',
+  url: 'https://www.yuque.com/yuqueyonghua2m9wj/web_food?#',
+  text: '前端资料 收集'
+}
+]
+// 后端资料
+export const backend =[{
+  name: '后端资料',
+  logo: 'logo.png',
+  url: 'https://dhc.pythonanywhere.com/article/public/1/',
+  text: '后端资料 收集'
+},
+{
+  name: 'Spring Cloud',
+  logo: 'spring.svg',
+  url: 'https://spring.io/projects/spring-cloud/',
+  text: '一套基于Spring Boot的微服务开发框架，旨在帮助开发者快速构建可扩展的分布式系统。'
+},
+{
+  name: 'Redis',
+  logo: 'redis.png',
+  url: 'http://redis.cn/',
+  text: 'Redis 是一个开源（BSD许可）的，内存中的数据结构存储系统，它可以用作数据库、缓存和消息中间件'
+}]
+export const Blog = [{
+  name: 'CSDN',
+  logo: 'csdn.ico',
+  url: 'https://www.csdn.net/',
+  text: '中国最大的开发者社区之一，提供了丰富的技术资源和交流平台，包括博客、论坛、问答、课程、云服务等多个板块。'
+},
+{
+  name: '稀土掘金',
+  logo: 'juejin.svg',
+  url: 'https://juejin.cn/',
+  text: '稀土掘金是面向全球中文开发者的技术内容分享与交流平台。'
+},
+{
+  name: 'Gitee',
+  logo: 'gitee.svg',
+  url: 'https://gitee.com/',
+  text: '基于Git的代码托管平台、企业级研发效能平台，提供中国本土化的代码托管服务。'
+},
+{
+  name: 'Github',
+  logo: 'github.png',
+  url: 'https://github.com/',
+  text: '基于Git的代码托管平台、企业级研发效能平台，提供中国本土化的代码托管服务。'
+}]
--- src/components/codes/index.vue
+++ src/components/codes/index.vue
@@ -2,18 +2,66 @@
   <div class="codes viewContainer">
     <div class="codesContent">
       <div class="codesTitle">
-        前端网站
+        博客论坛
       </div>
       <div class="codesList">
         <div
           class="codesItem"
-          v-for="(item,index) in arr"
+          v-for="(item,index) in Blog"
           :key="item"
           :class="'animated-fade-up-' + index"
           @click="to(item.url)"
         >
-          <h3>{{item.name}}</h3>
-          <p>{{item.text}}</p>
+          <img
+            :src="'/src/assets/images/'+item.logo"
+            alt=""
+          >
+          <div class="text">
+            <h3>{{item.name}}</h3>
+            <p>{{item.text}}</p>
+          </div>
+        </div>
+      </div>
+      <div class="codesTitle">
+        前端
+      </div>
+      <div class="codesList">
+        <div
+          class="codesItem"
+          v-for="(item,index) in frontEnd"
+          :key="item"
+          :class="'animated-fade-up-' + (Blog.length+index)"
+          @click="to(item.url)"
+        >
+          <img
+            :src="'/src/assets/images/'+item.logo"
+            alt=""
+          >
+          <div class="text">
+            <h3>{{item.name}}</h3>
+            <p>{{item.text}}</p>
+          </div>
+        </div>
+      </div>
+      <div class="codesTitle">
+        后端
+      </div>
+      <div class="codesList">
+        <div
+          class="codesItem"
+          v-for="(item,index) in backend"
+          :key="item"
+          :class="'animated-fade-up-' + (frontEnd.length+Blog.length+index)"
+          @click="to(item.url)"
+        >
+          <img
+            :src="'/src/assets/images/'+item.logo"
+            alt=""
+          >
+          <div class="text">
+            <h3>{{item.name}}</h3>
+            <p>{{item.text}}</p>
+          </div>
         </div>
       </div>
     </div>
@@ -22,42 +70,7 @@
 </template>
 
 <script setup>
-const arr = [{
-  name: 'Vue.js',
-  url: 'https://vuejs.org/',
-  text: 'Vue.js 是一款构建数据驱动的 web 界面的渐进式框架。与其他庞大的框架不同，Vue 被设计为可以自底向上逐层应用。Vue 的核心库只关注视图层，不仅易于上手，也便于与第三方库或已有项目整合'
-},
-{
-  name: 'React.js',
-  url: 'https://reactjs.org/',
-  text: 'React 是一个用于构建用户界面的 JavaScript 库。它由 Facebook 开发并开源，用于构建用户界面。React 可以使你构建大型、复杂的单页应用，而且只使用了 JavaScript 作为编程语言。'
-},
-
-{
-  name: 'Angular.js',
-  url: 'https://angular.io/',
-  text: 'Angular 是一个开源的 JavaScript 客户端应用框架，使用 TypeScript 语言进行开发。Angular 的目标是解决前端开发者在日常工作中遇到的问题，如模板的使用、脏检查和路由等。它使用了一个组件化的方法来构建单页应用，这使得开发、测试和重构变得更加容易。'
-},
-{
-  name: 'Webpack',
-  url: 'https://webpack.dev/',
-  text: 'Webpack 是一个静态模块打包器，主要用于将模块化的代码打包成浏览器可以执行的 JavaScript 文件。它能够处理图片、CSS、字体等资源文件，并支持热更新和代码分割等功能。Webpack 的配置灵活，可以满足各种复杂的需求。'
-},
-{
-  name: 'Axios',
-  url: 'https://axios-http.com/',
-  text: 'Axios 是一个基于 Promise 的 HTTP 客户端，可以在浏览器和 Node.js 中使用。它提供了一种简单的方法来发送 HTTP 请求，并支持拦截请求和响应、转换请求和响应数据等功能。Axios 还支持取消请求、自动转换 JSON 数据、客户端支持防止 CSRF 等特性。'
-},
-{
-  name: 'Element-ui',
-  url: 'https://element.eleme.cn/#/zh-CN',
-  text: '桌面端组件库'
-},
-{
-  name: 'Ant Design of React',
-  url: 'https://ant.design/docs/react/introduce-cn',
-  text: '企业级产品'
-}]
+import { frontEnd, backend, Blog } from './index.js'
 const to = (url) => {
   window.open(url)
 }
@@ -69,30 +82,71 @@ const to = (url) => {
   padding-top: 15px;
   .codesContent {
     border-radius: $BorderRadius;
-    height: 1000px;
+    padding-bottom: 50px;
+
     .codesTitle {
       margin: 30px 0 50px 0;
       font-size: 28px;
       text-align: center;
     }
     .codesList {
-      display: grid;
-      grid-template-columns: 22% 22% 22% 22%;
-      grid-template-rows: 180px 180px 180px 180px;
-      grid-gap: 4%;
+      display: flex;
+      flex-wrap: wrap;
       .codesItem {
-        padding: 20px;
+        height: 100px;
+        width: 23%;
+        box-sizing: border-box;
+        margin-right: 2%;
+        margin-bottom: 20px;
+        padding: 10px;
         background-color: #fff;
         box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.05);
+        display: flex;
+        align-items: center;
+        border-radius: 10px;
+        overflow: hidden;
+        cursor: pointer;
+        img {
+          width: 60px;
+          height: 60px;
+          margin-right: 15px;
+          transition: all 0.8s;
+        }
         &:nth-child(4n) {
           margin-right: 0 !important;
         }
-        p {
-          overflow: hidden;
-          text-overflow: ellipsis;
-          display: -webkit-box;
-          -webkit-line-clamp: 2;
-          -webkit-box-orient: vertical;
+        .text {
+          h3 {
+            margin-bottom: 5px;
+          }
+          p {
+            color: #99a9bf;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;
+          }
+        }
+        &:hover {
+          background-color: #64a15e;
+          // background-image: linear-gradient(to right, #fff, 50%, #64a15e);
+          img {
+            width: 0px;
+            height: 0px;
+            opacity: 0;
+          }
+          .text {
+            width: 100%;
+            // background-image: linear-gradient(to right, #64a15e, 30%, white);
+            // background-clip: text;
+            // -webkit-background-clip: text;
+            // -webkit-text-fill-color: transparent;
+            h3,
+            p {
+              color: #fff;
+            }
+          }
         }
       }
     }
