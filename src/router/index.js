02654663092354978640d2d398a3910524991bb5
‘jiaoyou’ <18898159853@163.com>
2024-01-19 17:29:26 +0800

    页面标题

--- src/router/index.js
+++ src/router/index.js
@@ -6,12 +6,14 @@ export const routes = [
     path: '/',
     name: 'Home',
     component:()=>import('../view/Home/index.vue'),
+    meta: { title: '首页' }
   },
   {
       path: '/article',
       name: 'article',
       component:()=>import('../view/article/index.vue'),
       redirect:'/article/index',
+      meta: { title: '随笔文章' },
       children:[
         {
           path:'index',
@@ -31,16 +33,19 @@ export const routes = [
       path: '/codes',
       name: 'codes',
       component:()=>import('../view/codes/index.vue'),
+      meta: { title: '网站分享' }
     },
     {
       path: '/demo',
       name: 'demo',
       component:()=>import('../view/demo/index.vue'),
+      meta: { title: 'Demo展示' }
     },
     {
       path: '/access',
       name: 'access',
       component:()=>import('../view/access/index.vue'),
+      meta: { title: '访客记录' }
     },
 ]
 
@@ -49,4 +54,8 @@ const router = createRouter({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  window.document.title = to.meta.title
+  next()
+})
 export default router
